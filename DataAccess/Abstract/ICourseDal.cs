4021bdeb5949c9a903acad953b86953893c5672c
Faruk KARAAASLAN <faruk.karaaslan.55@gmail.com>
2023-05-09 20:46:36 +0300

    feat: Add course request dto

--- DataAccess/Abstract/ICourseDal.cs
+++ DataAccess/Abstract/ICourseDal.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using Core.Persistens;
+using DataAccess.Contexts;
+using Entities.Concreate;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace DataAccess.Abstract;
+
+public interface ICourseDal:IAsyncRepository<Course,Guid>,IRepository<Course,Guid>
 {
-    internal interface ICourseDal
-    {
-    }
 }
