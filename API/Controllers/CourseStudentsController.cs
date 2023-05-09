b78e484e5d40ed312b229d1943b0200afeb62801
Faruk KARAAASLAN <faruk.karaaslan.55@gmail.com>
2023-05-09 21:22:04 +0300

    feat: add coursestudent controller

---
+++ API/Controllers/CourseStudentsController.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseStudentsController : ControllerBase
+    {
+        private ICourseStudentService courseStudentService;
+
+        public CourseStudentsController(ICourseStudentService courseStudentService)
+        {
+            this.courseStudentService = courseStudentService;
+        }
+
+        public async Task<IActionResult> Add([FromBody] CreateCourseStudentRequest createCourseStudentRequest)
+        {
+            await courseStudentService.Add(createCourseStudentRequest);
+            return Ok();
+        }
+    }
+}
